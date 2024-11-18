d3fda8615a0d93627c0378e4620b3d95023d0f14
Lucas Amsellem <lucas.amsellem21@gmail.com>
2024-11-18 13:48:27 +0100

    close popup when clicking outside of it

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const havePopups = document.querySelectorAll('.has-popup');
 
-function togglePopupInfo(container, btn) {
-  const isHidden = container.classList.toggle('hidden');
-  btn.style.opacity = isHidden ? '0.2' : '0.8';
-}
-
 havePopups.forEach((project) => {
   const infoBtn = project.querySelector('.info-icon');
   const projectInfo = project.querySelector('.popup');
 
-  infoBtn.addEventListener('click', () =>
-    togglePopupInfo(projectInfo, infoBtn)
-  );
+  infoBtn.addEventListener('click', (e) => {
+    const isHidden = projectInfo.classList.toggle('hidden');
+    infoBtn.style.opacity = isHidden ? '0.2' : '0.8';
+    e.stopPropagation();
+  });
+
+  // Close popup when clicking outside of it
+  window.addEventListener('click', (e) => {
+    if (!projectInfo.contains(e.target)) {
+      projectInfo.classList.add('hidden');
+      infoBtn.style.opacity = '0.2';
+    }
+  });
+
+  // Prevent clicks inside the popup from closing it
+  projectInfo.addEventListener('click', (e) => e.stopPropagation());
 });
