4d3b51455c94f6b6db2bdbedf184c943b3795666
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-01-09 14:45:56 +0100

    added Next.js skill

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const havePopups = document.querySelectorAll('.has-popup');
 
 havePopups.forEach((project) => {
   const infoBtn = project.querySelector('.info-icon');
-  const projectInfo = project.querySelector('.popup');
+  const popup = project.querySelector('.popup');
 
   infoBtn.addEventListener('click', (e) => {
-    const isHidden = projectInfo.classList.toggle('hidden');
+    const isHidden = popup.classList.toggle('hidden');
     infoBtn.style.opacity = isHidden ? '0.2' : '0.8';
     e.stopPropagation();
   });
 
   // Close popup when clicking outside of it
   window.addEventListener('click', (e) => {
-    if (!projectInfo.contains(e.target)) {
-      projectInfo.classList.add('hidden');
+    if (!popup.contains(e.target)) {
+      popup.classList.add('hidden');
       infoBtn.style.opacity = '0.2';
     }
   });
 
   // Prevent clicks inside the popup from closing it
-  projectInfo.addEventListener('click', (e) => e.stopPropagation());
+  popup.addEventListener('click', (e) => e.stopPropagation());
 });
